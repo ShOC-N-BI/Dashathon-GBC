8d7eccfc42ea96852990de676522eea8bce9f00f
Ernest Carrasco <ernest3443@yahoo.com>
2025-09-18 10:33:35 -0700

    w

--- dbc_app/sequence.py
+++ dbc_app/sequence.py
@@ -6,22 +6,28 @@ def make_timeline(friendly, results_hostiles, results_fuel, results_support, tim
     time = dt.strftime("%H%M")
     support_time = (dt + timedelta(minutes=10)).strftime("%H%M")
     mission_time = (dt + timedelta(minutes=20)).strftime("%H%M")
-
+    asset = friendly["callsign"]
     tanker = results_fuel["tanker_callsign"]
-    support = results_support["escort"]["callsign"]
+    if results_support and "escort" in results_support:
+      support = results_support["escort"].get("callsign", "")
+
+    else:
+      support = ""
+
+
     fuel_score = results_fuel["score"]
     hostile_score = results_hostiles[0]
 
     if fuel_score == 4 and hostile_score == 4:
-        return f"Push {friendly} {time}"
+        return f"Push {asset} {time}"
     
     if fuel_score in (2, 3) and hostile_score == 4:
-        return f"Push {tanker} {time}, Push {friendly} {support_time}"
+        return f"Push {tanker} {time}, Push {asset} {support_time}"
     
     if fuel_score == 4 and hostile_score < 4:
-        return f"Push {support} {time}, Push {friendly} {support_time}"
+        return f"Push {support} {time}, Push {asset} {support_time}"
     
     if fuel_score in (2, 3) and hostile_score < 4:
-        return f"Push {tanker} {time}, Push {support} {support_time}, Push {friendly} {mission_time}"
+        return f"Push {tanker} {time}, Push {support} {support_time}, Push {asset} {mission_time}"
     
-    return None
+    return "N/A"
