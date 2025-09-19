1a1c584d321f87879acca1791ba4a7fcf7709fc5
Carlos Dye II <62572729+Incrision@users.noreply.github.com>
2025-09-19 14:43:19 -0700

    working again

--- dbc_app/app.py
+++ dbc_app/app.py
@@ -76,7 +76,7 @@ def evaluate_aircraft(friendly, target, message, timestamp):
     #7. Assess risk and Build 5-Line
      
     results = fiveline.generate(results_amament, results_hostiles, results_fuel, results_time, results_support, results_sequence, message, friendly, target)
-    print("results")
+    print(results)
     print("WORKS")
 
     return results
